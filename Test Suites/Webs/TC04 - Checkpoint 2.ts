62aec4ded12dc36ef040a6f872ff0329def1e8af
kuxuluvu <vuduc762@gmail.com>
2024-06-16 21:58:32 +0700

    add cp2

---
+++ Test Suites/Webs/TC04 - Checkpoint 2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC04 - Checkpoint 2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d9bf1caf-528b-414f-a777-f7d343fca2f6</testSuiteGuid>
+   <testCaseLink>
+      <guid>37afdb8d-54fd-4fc4-9434-4d638df5c7a0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Webs/TC04 - Checkpoint2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
